194dd159ca06ebf500e7aca74b9782807dca4080
Ayush Kumar Srivastava <ayush050419.com>
2023-04-24 00:30:14 +0530

    24-04-23

--- tri3.cpp
+++ tri3.cpp
@@ -1,1 +1,44 @@
-     
+    //selection sorting 
+    #include<bits/stdc++.h>
+    using namespace std;
+    void enter(int a[],int n){
+        cout<<"Enter the elements\n";
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+    }
+    void disp(int a[],int n){
+        cout<<"The elements are:\n";
+        for(int i=0;i<n;i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+    void selectionsort(int a[],int n){
+        int i,j,k,c;
+        for(i=0;i<n-1;i++){
+            k=i;
+            for(j=i+1;j<n;j++){
+                if(a[k]>a[j]){
+                    k=j;
+                }
+            }
+            if(k!=i){
+                c=a[i];
+                a[i]=a[k];
+                a[k]=c;
+            }
+        }
+        
+    }
+    int main(){
+        int n;
+        cout<<"Enter the number of elements inn the array\n";
+        cin>>n;
+        int a[n];
+        enter(a,n);
+        disp(a,n);
+        selectionsort(a,n);
+        cout<<"The sorted array is:::: \n";
+        disp(a,n);
+    }
--- trial.cpp
+++ trial.cpp
@@ -1,26 +1,44 @@
-
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    char text[20],pat[20];
-    int a,b;
-    cout<<"Enter the text :";
-    gets(text);
-    cout<<"Enter the pattern :";
-    gets(pat);
-    a=strlen(pat);
-    b=strlen(text);
-    for(int i=0;i<=b-a;i++){
-        int j;
-        for(j=0;j<a;j++){
-            if(text[i+j]!=pat[j]){
-                break;
+void enter(int a[],int n){
+    cout<<"Enter the values in t he array "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+}
+void disp(int a[],int n){
+    cout<<"The elements of the array are"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+void selectionsort(int a[],int n){
+    int i,j,k,c;
+    for(i=0;i<n-1;i++){
+        k=i;
+        for(j=i+1;j<n;j++){
+            if(a[k]>a[j]){
+                k=j;
             }
         }
-        if(j==a){
-            cout<<"Found at position "<<i+1<<endl;
+        if(k!=i){
+            c=a[i];
+            a[i]=a[k];
+            a[k]=c;
         }
     }
+}
+int main(){
+    int n;
+    cout<<"Enter the number of the elements"<<endl;
+    cin>>n;
+    int a[n];
+    enter(a,n);
+    cout<<"1.Elements before sorting::\n";
+    disp(a,n);
+    selectionsort(a,n);
+    cout<<"2.Elements after sorting1::\n";
+    disp(a,n);
     return 0;
 }
-
